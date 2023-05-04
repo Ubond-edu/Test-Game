1e74dd454d13efd16116b93ec2b261a5890f0895
Ubond-edu <132318268+Ubond-edu@users.noreply.github.com>
2023-05-04 16:57:47 +0800

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,29 @@
+// Include the jQuery library
+if (!window.jQuery) {
+  let script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
+  document.head.appendChild(script);
+}
+
+window.startTimer = function (elementId, callback) {
+  let remainingSeconds = 120;
+  const timerElement = document.getElementById(elementId);
+
+  const interval = setInterval(() => {
+    remainingSeconds--;
+    timerElement.textContent = remainingSeconds + " seconds";
+
+    if (remainingSeconds <= 0) {
+      clearInterval(interval);
+      callback();
+    }
+  }, 1000);
+
+  return interval;
+};
+
+window.stopTimer = function (intervalId) {
+  clearInterval(intervalId);
+};
 
